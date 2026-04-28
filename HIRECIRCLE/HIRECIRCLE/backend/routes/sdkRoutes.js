d175a9de1b593651dd2272c1724d7e1f7b2d166d
Shesheer <shesheer16@gmail.com>
2026-04-28 16:48:46 +0530

    refactor: completely cleaned repository structure for the team

--- HIRECIRCLE/HIRECIRCLE/backend/routes/sdkRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { serveHireSdkV1 } = require('../controllers/sdkController');
-
-const router = express.Router();
-
-router.get('/hire-sdk.v1.js', serveHireSdkV1);
-
-module.exports = router;
